4558cdebfa5ecb8922191eda3245d7b60f7730f1
joshevensen <email@joshevensen.com>
2014-08-10 13:54:14 -0500

    initial setup

--- app/config/database.php
+++ app/config/database.php
@@ -46,42 +46,16 @@ return array(
 
 	'connections' => array(
 
-		'sqlite' => array(
-			'driver'   => 'sqlite',
-			'database' => __DIR__.'/../database/production.sqlite',
-			'prefix'   => '',
-		),
-
-		'mysql' => array(
-			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
-			'charset'   => 'utf8',
-			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
-		),
-
-		'pgsql' => array(
-			'driver'   => 'pgsql',
-			'host'     => 'localhost',
-			'database' => 'forge',
-			'username' => 'forge',
-			'password' => '',
-			'charset'  => 'utf8',
-			'prefix'   => '',
-			'schema'   => 'public',
-		),
-
-		'sqlsrv' => array(
-			'driver'   => 'sqlsrv',
-			'host'     => 'localhost',
-			'database' => 'database',
-			'username' => 'root',
-			'password' => '',
-			'prefix'   => '',
-		),
+    'mysql' => array(
+      'driver'    => 'mysql',
+      'host'      => getenv('DB_HOST'),
+      'database'  => getenv('DB_NAME'),
+      'username'  => getenv('DB_USERNAME'),
+      'password'  => getenv('DB_PASSWORD'),
+      'charset'   => 'utf8',
+      'collation' => 'utf8_unicode_ci',
+      'prefix'    => '',
+    ),
 
 	),
 
